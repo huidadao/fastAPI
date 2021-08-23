643570997d80a2d9680e58d05e49689889e95517
huidadao <huidadao@gmail.com>
2021-08-23 14:02:35 +0900

    create crud

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from schemas import Blog
 
+from database import SessionLocal, engine
+
 app = FastAPI()
+
+# Dependency
+def get_db():
+  db = SessionLocal()
+  try:
+      yield db
+  finally:
+      db.close()
 
 @app.post('/blog')
 def create(blog: Blog):
